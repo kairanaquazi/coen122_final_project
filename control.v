7a9625eba977bfbc70b698cfe30851c367d2748f
kairanaquazi <48654706+kairanaquazi@users.noreply.github.com>
2022-05-31 21:21:59 -0700

    Update control.v

--- control.v
+++ control.v
@@ -1,19 +1,19 @@
 `timescale 1ns / 1ps
 
-module control(input [31:0] inst, output reg svpc, output reg brz, output reg brn, output reg j, output reg wai, output reg memw, output reg memr, output reg regw, output reg alusrc, output reg [2:0] aluop) begin
+module control(input [31:0] inst, output wire svpc, output wire brz, output wire brn, output wire j, output wire wai, output wire memw, output wire memr, output wire wirew, output wire alusrc, output wire [2:0] aluop);
 wire [3:0] opcode;
-opcode=inst[31:28];
-svpc=opcode[0]&opcode[1]&opcode[2]&opcode[3];
-brz=opcode[0]&~opcode[1]&~opcode[2]&opcode[3];
-brn = opcode[0] & opcode[1] & ~opcode[2] & opcode[3]
-j = ~opcode[0] & ~opcode[1] & ~opcode[2] & opcode[3]
-wai = ~opcode[0] & opcode[1] & ~opcode[2] & opcode[3]
-memw = opcode[0] & opcode[1] & ~opcode[2] & ~opcode[3]
-memr = ~opcode[0] & opcode[1] & opcode[2] & opcode[3]
-regw = opcode[2] | (~opcode[0] & opcode[1] & opcode[3])
-alusrc = (opcode[0] & opcode[1] & opcode[2] & opcode[3]) | (opcode[0] & ~opcode[1] & opcode[2] & ~opcode[3]) 
-aluop[0] = (~opcode[1] & opcode[2] & ~opcode[3]) | (opcode[0] & opcode[1] & opcode[2] & opcode[3])
-aluop[1] = ~opcode[0] & opcode[1] & opcode[2] & ~opcode[3]
-aluop[2] = opcode[0] & opcode[1] & opcode[2] & ~opcode[3]
+assign opcode=inst[31:28];
+assign svpc=opcode[0]&opcode[1]&opcode[2]&opcode[3];
+assign brz=opcode[0]&~opcode[1]&~opcode[2]&opcode[3];
+assign brn = opcode[0] & opcode[1] & ~opcode[2] & opcode[3];
+assign j = ~opcode[0] & ~opcode[1] & ~opcode[2] & opcode[3];
+assign wai = ~opcode[0] & opcode[1] & ~opcode[2] & opcode[3];
+assign memw = opcode[0] & opcode[1] & ~opcode[2] & ~opcode[3];
+assign memr = ~opcode[0] & opcode[1] & opcode[2] & opcode[3];
+assign regw = opcode[2] | (~opcode[0] & opcode[1] & opcode[3]);
+assign alusrc = (opcode[0] & opcode[1] & opcode[2] & opcode[3]) | (opcode[0] & ~opcode[1] & opcode[2] & ~opcode[3]) ;
+assign aluop[0] = (~opcode[1] & opcode[2] & ~opcode[3]) | (opcode[0] & opcode[1] & opcode[2] & opcode[3]);
+assign aluop[1] = ~opcode[0] & opcode[1] & opcode[2] & ~opcode[3];
+assign aluop[2] = opcode[0] & opcode[1] & opcode[2] & ~opcode[3];
 
 endmodule
