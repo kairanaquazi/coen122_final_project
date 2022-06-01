6f520aa404b0b0ab61b519adaa3779344565ea3e
Kairan Quazi <kairan.quazi@gmail.com>
2022-05-31 17:23:27 -0700

    control.v added

---
+++ control.v
@@ -0,0 +1,19 @@
+`timescale 1ns / 1ps
+
+module(input [31:0] inst, output reg svpc, output reg brz, output reg brn, output reg j, output reg wai, output reg memw, output reg memr, output reg regw, output reg alusrc, output reg [2:0] aluop) begin
+wire [3:0] opcode;
+opcode=inst[31:28];
+svpc=opcode[0]&opcode[1]&opcode[2]&opcode[3];
+brz=opcode[0]&~opcode[1]&~opcode[2]&opcode[3];
+brn = opcode[0] & opcode[1] & ~opcode[2] & opcode[3]
+j = ~opcode[0] & ~opcode[1] & ~opcode[2] & opcode[3]
+wai = ~opcode[0] & opcode[1] & ~opcode[2] & opcode[3]
+memw = opcode[0] & opcode[1] & ~opcode[2] & ~opcode[3]
+memr = ~opcode[0] & opcode[1] & opcode[2] & opcode[3]
+regw = opcode[2] | (~opcode[0] & opcode[1] & opcode[3])
+alusrc = (opcode[0] & opcode[1] & opcode[2] & opcode[3]) | (opcode[0] & ~opcode[1] & opcode[2] & ~opcode[3]) 
+aluop[0] = (~opcode[1] & opcode[2] & ~opcode[3]) | (opcode[0] & opcode[1] & opcode[2] & opcode[3])
+aluop[1] = ~opcode[0] & opcode[1] & opcode[2] & ~opcode[3]
+aluop[2] = opcode[0] & opcode[1] & opcode[2] & ~opcode[3]
+
+endmodule
--- idecode.v
+++ idecode.v
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 `include "regfile.v"
 
-module idecode(input clk, input svpc, input [31:0]  pci, input [31:0] inst, input rw, input [31:0] wd, input [5:0] rdi, output reg [31:0] pco, output reg [31:0] lhs, output reg [5:0] rd2o, output reg [31:0] imm, output [5:0] rdo) begin
+module idecode(input clk, input svpc, input [31:0]  pci, input [31:0] inst, input rw, input [31:0] wd, input [5:0] rdi, output reg [31:0] pco, output reg [31:0] lhs, output reg [5:0] rd2o, output reg [31:0] imm, output reg [5:0] rdo) begin
 wire [31:0] rd1o;
 regfile urf(rdi,inst[21:16],inst[15:10], wd, clk, rw, rd1o, rd2o);
 always @(posedge clk) begin
