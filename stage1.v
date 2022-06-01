cb17b8b6bade6f5aed8364d0f0670e14cea920e1
Kairan Quazi <kairan.quazi@gmail.com>
2022-05-31 21:40:43 -0700

    a

--- stage1.v
+++ stage1.v
@@ -3,7 +3,11 @@
 `include "IF_ID_buffer.v"
 
 module stage1 (
-    ports
-);
-    
+    input clk, input [5:0] rd1, input brz, input brn, input j, output reg [31:0] out_instruction, output reg [31:0] out_PC);
+
+    reg [31:0] PC;
+    reg [31:0] instruction;
+    ifetch fetcher(clk, rd1, brz, brn, j, PC, instruction);
+    IF_ID_buffer buffer(clk, PC, instruction, out_instruction, out_PC);
+
 endmodule
