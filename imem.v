0736db4a10a43102d2918c6cd5106f2caaea2b6d
kairanaquazi <48654706+kairanaquazi@users.noreply.github.com>
2022-05-31 21:24:02 -0700

    Update imem.v

--- imem.v
+++ imem.v
@@ -1,13 +1,9 @@
 `timescale 1ns / 1ps
 
 
-module imem(input clock, input [7:0] addr, input [31:0] inp [255:0], output reg [31:0] inst);
-wire [31:0] data [255:0];
-wire w;
-assign w=0;
-
+module imem(input clock, input [7:0] addr, output reg [31:0] inst);
+reg [31:0] data [255:0];
 always @(posedge clock) begin
-if(w==0) begin data=inp; w=1; end
 inst=data[addr];
 end
 endmodule
