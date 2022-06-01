0ada227bb9143206865536ddbac291433372106a
Jordan Randleman <jordanran199@gmail.com>
2022-05-31 21:02:39 -0700

    Update control.v

--- control.v
+++ control.v
@@ -1,6 +1,6 @@
 `timescale 1ns / 1ps
 
-module(input [31:0] inst, output reg svpc, output reg brz, output reg brn, output reg j, output reg wai, output reg memw, output reg memr, output reg regw, output reg alusrc, output reg [2:0] aluop) begin
+module control(input [31:0] inst, output reg svpc, output reg brz, output reg brn, output reg j, output reg wai, output reg memw, output reg memr, output reg regw, output reg alusrc, output reg [2:0] aluop) begin
 wire [3:0] opcode;
 opcode=inst[31:28];
 svpc=opcode[0]&opcode[1]&opcode[2]&opcode[3];
