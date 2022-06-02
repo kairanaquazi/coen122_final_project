89bb7f362afcc4e05e49cc169d5a383438babbfd
Kairan Quazi <kairan.quazi@gmail.com>
2022-06-02 13:57:51 -0700

    message

--- max.asm
+++ max.asm
@@ -1,22 +1,25 @@
-SUB x0, x0, x0
+SUB  x0, x0, x0
 NOP
 ADDi x4, x0, -65535
-ADD x5, x2, x3
+ADD  x5, x2, x3
 NOP
 ADDi x5, x5, 1
 SVPC x9, 2
-SVPC x10, 8
+SVPC x10, 10
 LD x6, x2
 NOP
-SUB x7, x6, x4
-BRN x10
+SUB  x7, x6, x4
 NOP
 NOP
-ADD x4, x6, x0
+BRN  x10
+NOP
+NOP
+ADD  x4, x6, x0
 ADDi x2, x2, 1
 NOP
-SUB x7, x5, x2
-BRZ x9
+SUB  x7, x5, x2
 NOP
 NOP
-ADDi x4, x4, 0
+BRZ  x9
+NOP
+NOP
