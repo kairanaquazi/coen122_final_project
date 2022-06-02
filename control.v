341ba67c0d2dcf8f08e39b21c5385a537827782f
Matthew <74221209+MatthewBrunkhorst@users.noreply.github.com>
2022-06-01 18:24:35 -0700

    Update control.v

--- control.v
+++ control.v
@@ -1,11 +1,11 @@
 `timescale 1ns / 1ps
 
-module control(input [31:0] inst, output wire svpc, output wire brz, output wire brn, output wire j, output wire wai, output wire memw, output wire memr, output wire wirew, output wire alusrc, output wire [2:0] aluop);
+module control(input [31:0] inst, input z, input n, output wire svpc, output wire brz, output wire brn, output wire j, output wire wai, output wire memw, output wire memr, output wire wirew, output wire alusrc, output wire [2:0] aluop);
 wire [3:0] opcode;
 assign opcode=inst[31:28];
 assign svpc=opcode[0]&opcode[1]&opcode[2]&opcode[3];
-assign brz=opcode[0]&~opcode[1]&~opcode[2]&opcode[3];
-assign brn = opcode[0] & opcode[1] & ~opcode[2] & opcode[3];
+assign brz=(opcode[0]&~opcode[1]&~opcode[2]&opcode[3]) & z;
+assign brn = (opcode[0] & opcode[1] & ~opcode[2] & opcode[3]) & n;
 assign j = ~opcode[0] & ~opcode[1] & ~opcode[2] & opcode[3];
 assign wai = ~opcode[0] & opcode[1] & ~opcode[2] & opcode[3];
 assign memw = opcode[0] & opcode[1] & ~opcode[2] & ~opcode[3];
