a884216a1eb421e50692f0f583e247201e4ddb55
Matthew <74221209+MatthewBrunkhorst@users.noreply.github.com>
2022-06-02 15:47:48 -0700

    Update control.v

--- control.v
+++ control.v
@@ -1,6 +1,6 @@
 `timescale 1ns / 1ps
 
-module control(input [31:0] inst, input z, input n, output wire svpc, output wire brz, output wire brn, output wire j, output wire wai, output wire memw, output wire memr, output wire wirew, output wire alusrc, output wire [2:0] aluop);
+module control(input [31:0] inst, input z, input n, output wire svpc, output wire brz, output wire brn, output wire j, output wire wai, output wire memw, output wire memr, output wire regw, output wire alusrc, output wire [2:0] aluop);
 wire [3:0] opcode;
 assign opcode=inst[31:28];
 assign svpc=opcode[0]&opcode[1]&opcode[2]&opcode[3];
@@ -12,8 +12,8 @@ assign memw = opcode[0] & opcode[1] & ~opcode[2] & ~opcode[3];
 assign memr = ~opcode[0] & opcode[1] & opcode[2] & opcode[3];
 assign regw = opcode[2] | (~opcode[0] & opcode[1] & opcode[3]);
 assign alusrc = (opcode[0] & opcode[1] & opcode[2] & opcode[3]) | (opcode[0] & ~opcode[1] & opcode[2] & ~opcode[3]) ;
-assign aluop[0] = (~opcode[1] & opcode[2] & ~opcode[3]) | (opcode[0] & opcode[1] & opcode[2] & opcode[3]);
+assign aluop[2] = (~opcode[1] & opcode[2] & ~opcode[3]) | (opcode[0] & opcode[1] & opcode[2] & opcode[3]); //FLIPPED TO FIX SAVE PC
 assign aluop[1] = ~opcode[0] & opcode[1] & opcode[2] & ~opcode[3];
-assign aluop[2] = opcode[0] & opcode[1] & opcode[2] & ~opcode[3];
+assign aluop[0] = opcode[0] & opcode[1] & opcode[2] & ~opcode[3]; //FLIPPED TO FIX SAVE PC
 
 endmodule
