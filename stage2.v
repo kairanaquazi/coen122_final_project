5d014694a7168958cd990af72c2b3748ddeb5174
Matthew <74221209+MatthewBrunkhorst@users.noreply.github.com>
2022-06-01 18:24:12 -0700

    Update stage2.v

--- stage2.v
+++ stage2.v
@@ -3,14 +3,14 @@
 `include "ID_EX_buffer.v"
 `include "control.v"
 
-module stage2(input clk, input [31:0] PC, input [31:0] instruction, input [31:0] writedata, input [5:0] rdi, output reg [31:0] out_imm, output reg [5:0] out_rd, output reg [31:0] out_rd1, output reg [31:0] out_rd2, output reg [31:0] out_PC, output reg out_brz, output reg out_brn, output reg out_j, output reg out_regw, output reg out_wai, output reg out_memw, output reg out_memr, output reg out_alusrc, output reg [2:0] out_aluop);
+module stage2(input clk, input [31:0] PC, input [31:0] instruction, input zero, input neg, input [31:0] writedata, input [5:0] rdi, output reg [31:0] out_imm, output reg [5:0] out_rd, output reg [31:0] out_rd1, output reg [31:0] out_rd2, output reg [31:0] out_PC, output reg out_brz, output reg out_brn, output reg out_j, output reg out_regw, output reg out_wai, output reg out_memw, output reg out_memr, output reg out_alusrc, output reg [2:0] out_aluop);
 
 wire svpc, brz, brn, j, wai, memw, memr, regw, alusrc;
 wire [2:0] aluop;
 wire [5:0] rd0;
 wire [31:0] pco, lhs, rd2o, imm;
 
-control control_instance(instruction, svpc, brz, brn, j, wai, memw, memr, regw, alusrc, aluop);
+control control_instance(instruction, zero, neg, svpc, brz, brn, j, wai, memw, memr, regw, alusrc, aluop);
 
 idecode idecode_instance(clk, svpc, PC, instruction, regw, writedata, rdi, pco, lhs, rd2o, imm, rdo);
 
